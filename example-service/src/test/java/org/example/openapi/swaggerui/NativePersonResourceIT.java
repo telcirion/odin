43dd2389b563ec078ba55cb1f2c9d80d3fd2ac32
Peter <29709544+telcirion@users.noreply.github.com>
2020-06-26 13:37:07 +0200

    Setup Quarkus

---
+++ example-service/src/test/java/org/example/openapi/swaggerui/NativePersonResourceIT.java
@@ -0,0 +1,9 @@
+package org.example.openapi.swaggerui;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativePersonResourceIT extends PersonResourceTest {
+
+    // Execute the same tests but in native mode.
+}

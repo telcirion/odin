54bae7d1be0b11edcd791ada68478a8f898627d3
Peter <29709544+telcirion@users.noreply.github.com>
2021-02-02 11:23:43 +0100

    Fix warning

--- example/src/main/java/odin/example/infrastructure/openapi/PersonResource.java
+++ example/src/main/java/odin/example/infrastructure/openapi/PersonResource.java
@@ -3,7 +3,6 @@ package odin.example.infrastructure.openapi;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;

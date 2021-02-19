b7d8801d0e04e57db64ac92a6b1bdea32e2031b5
Peter <29709544+telcirion@users.noreply.github.com>
2021-02-19 11:22:48 +0100

    Improve coverage and upgrade checkstyle

--- example/src/test/java/odin/example/infrastructure/openapi/PersonResourceTest.java
+++ example/src/test/java/odin/example/infrastructure/openapi/PersonResourceTest.java
@@ -13,4 +13,15 @@ class PersonResourceTest {
     void testPersonGetEndpoint() {
         given().when().get("/persons").then().statusCode(200).body(is("{\"name\":null,\"ssn\":null}"));
     }
+
+    @Test
+    void testPersonPostRegisterPersonEndpoint() {
+        given().when().post("/persons/registrations").then().statusCode(415);
+    }
+
+    @Test
+    void testPersonPostChangePersonNameEndpoint() {
+        given().when().post("/persons/namechanges").then().statusCode(415);
+    }
+
 }

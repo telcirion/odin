9587ca7ab2463fa3b05636aea03d780df920c582
Peter <29709544+telcirion@users.noreply.github.com>
2021-04-12 10:18:52 +0200

    clean up

--- framework/src/main/java/odin/framework/infrastructure/SqlEventStore.java
+++ framework/src/main/java/odin/framework/infrastructure/SqlEventStore.java
@@ -19,7 +19,6 @@ import java.lang.invoke.MethodHandles;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 

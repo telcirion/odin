7dfe758317aaea3cb6251f572a53112c2c559c9a
peter <29709544+telcirion@users.noreply.github.com>
2023-10-03 17:07:51 +0200

    add spring boot to example

---
+++ example/src/main/java/odin/example/ExampleApplication.java
@@ -0,0 +1,16 @@
+package odin.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleApplication {
+    private static final Logger log = LoggerFactory.getLogger(ExampleApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleApplication.class, args);
+        log.info("Up and running!");
+    }
+}

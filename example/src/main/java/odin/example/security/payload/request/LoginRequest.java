8312290a30c4f60e1a19164936559849d0a4e42c
peter <29709544+telcirion@users.noreply.github.com>
2024-07-01 09:17:09 +0200

    chore: Add Lombok annotations to request classes

--- example/src/main/java/odin/example/security/payload/request/LoginRequest.java
+++ example/src/main/java/odin/example/security/payload/request/LoginRequest.java
@@ -1,27 +1,15 @@
 package odin.example.security.payload.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class LoginRequest {
 	@NotBlank
 	private String username;
 
 	@NotBlank
 	private String password;
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 }
--- example/src/main/java/odin/example/security/payload/request/SignupRequest.java
+++ example/src/main/java/odin/example/security/payload/request/SignupRequest.java
@@ -5,7 +5,11 @@ import java.util.Set;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class SignupRequest {
   @NotBlank
   @Size(min = 3, max = 20)
@@ -22,35 +26,4 @@ public class SignupRequest {
   @Size(min = 6, max = 40)
   private String password;
 
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public Set<String> getRole() {
-    return this.role;
-  }
-
-  public void setRole(Set<String> role) {
-    this.role = role;
-  }
 }

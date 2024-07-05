f845793d795cb515a84afc925fd217e024071b44
peter <29709544+telcirion@users.noreply.github.com>
2024-07-05 18:43:10 +0200

    chore: Remove unused equals and hashCode methods in UserDetailsImpl

--- example/src/main/java/odin/example/security/services/UserDetailsImpl.java
+++ example/src/main/java/odin/example/security/services/UserDetailsImpl.java
@@ -2,7 +2,6 @@ package odin.example.security.services;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
@@ -76,20 +75,4 @@ public class UserDetailsImpl implements UserDetails {
         return true;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        UserDetailsImpl user = (UserDetailsImpl) o;
-        return Objects.equals(id, user.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
